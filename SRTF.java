d3dffb496886a2c0f9ce1df7e62b62e74cefd395
GpMarrecoTeteu <43113324+GpMarrecoTeteu@users.noreply.github.com>
2018-09-10 11:39:53 -0300

    Enviado por Matheus

--- SRTF.java
+++ SRTF.java
@@ -6,17 +6,17 @@ public class SRTF {
  {
   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       int n;
-      System.out.println("Por favor digite o número de processos: ");
+      System.out.println("Por favor digite o nÃºmero de processos: ");
        n = Integer.parseInt(br.readLine());
        int proc[][] = new int[n + 1][4];
        for(int i = 1; i <= n; i++)
        {
-      System.out.println("Por favor, informe o horário de chegada do processo " + i + ": ");
+      System.out.println("Por favor, informe o horÃ¡rio de chegada do processo " + i + ": ");
       proc[i][0] = Integer.parseInt(br.readLine());
       System.out.println("Por favor, informe o tempo de burst para o processo " + i + ": ");
       proc[i][1] = Integer.parseInt(br.readLine());
      }
-       System.out.println();
+  System.out.println();
      
        int total_time = 0;
      for(int i = 1; i <= n; i++)
@@ -40,58 +40,3 @@ public class SRTF {
         }
        }
       }
-                 time_chart[i] = sel_proc;
-                 proc[sel_proc][1]--;
-      
-            for(int j = 1; j <= n; j++)
-      {
-       if(proc[j][0] <= i)
-       {
-        if(proc[j][1] != 0)
-        {
-         proc[j][3]++;
-            if(j != sel_proc)
-             proc[j][2]++;
-        }
-        else if(j == sel_proc)
-         proc[j][3]++;
-       }
-      }
-                 if(i != 0)
-      {
-       if(sel_proc != time_chart[i - 1])
-               {
-        System.out.print("--" + i + "--P" + sel_proc);
-       }
-      }
-      else
-       System.out.print(i + "--P" + sel_proc);
-      if(i == total_time - 1)
-       System.out.print("--" + (i + 1));
-      
-     }
-     System.out.println();
-     System.out.println();
-     
-          System.out.println("P\t WT \t TT ");
-     for(int i = 1; i <= n; i++)
-     {
-      System.out.printf("%d\t%2dms\t%2dms",i,proc[i][2],proc[i][3]);
-      System.out.println();
-     }
-     
-     System.out.println();
-     
-     float WT = 0,TT = 0;
-     for(int i = 1; i <= n; i++)
-     {
-      WT += proc[i][2];
-      TT += proc[i][3];
-     }
-     WT /= n;
-     TT /= n;
-     System.out.println("The Average WT is: " + WT + "ms");
-     System.out.println("The Average TT is: " + TT + "ms");
- }
-    
-}
